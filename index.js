42f67ef65ec449f0af770ec9fb1b8bd3ddb0ffc6
Jesse Gabriel <jgabriel@hiperwall.com>
2018-09-07 09:41:20 -0700

    Added clearWall button

--- index.js
+++ index.js
@@ -11,20 +11,77 @@ $(document).ready(function () {
     });
 });
 
+// Temporary clear button
+// $("button").click(function () {
+//     $("div").empty();
+// });
+
 // Sends the XMLCommand to HiperService
-function loadXMLCommand() {
-  
+// function loadXMLCommand() {
+
+//     var header = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><Commands>";
+//     var openCmd = "<command type=\"open\"><name>Toyota 47.jpg.tif</name><id>image1</id></command>";
+//     var footer = "</Commands>";
+//     var xhttp = new XMLHttpRequest();
+
+//     xhttp.addEventListener("readystatechange", function () {
+//         if (this.readyState === 4) {
+//             console.log(this.responseText);
+//         }
+//     });
+
+//     xhttp.open("POST", "http://10.1.0.138:8000/xmlcommand", true);
+//     xhttp.send(header + openCmd + footer);
+// }
+
+function allowDrop(evt) {
+    evt.preventDefault();
+    console.log("item being dragged to valid location...");
+}
+
+function drag(evt) {
+    evt.dataTransfer.setData("key", evt.target.id);
+    console.log("it's dragging...");
+}
+
+function drop(evt) {
+
+    var data = evt.dataTransfer.getData("key");
+    evt.preventDefault();
+    evt.target.appendChild(document.getElementById(data));
+    console.log("Item dropped");
+
     var header = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><Commands>";
     var openCmd = "<command type=\"open\"><name>Toyota 47.jpg.tif</name><id>image1</id></command>";
     var footer = "</Commands>";
     var xhttp = new XMLHttpRequest();
-  
+
     xhttp.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
             console.log(this.responseText);
         }
     });
-  
+
     xhttp.open("POST", "http://10.1.0.138:8000/xmlcommand", true);
     xhttp.send(header + openCmd + footer);
-  }
+}
+
+function clearWall() {
+
+    $("button").click(function () {
+        $("div").empty();
+    });
+
+    var data = null;
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("GET", "http://localhost:8000/clearall");
+    xhr.send(data);
+}
